e138de3bc7b28380d187800f6f7f9710c92aaf7f
DevOsi123 <kathymeyer111@otlook.com>
2024-05-02 21:28:49 +0100

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+
+// let firstCard = 11;
+// let  secondCard = 9;
+// let sum = firstCard + secondCard;
+
+// let hasBlackJack = false;
+
+// let isAlive = true 
+
+// if ( sum <= 20){
+//     console.log("Do you want to draw a new card? 🙂");
+//     isAlive = false;
+// }
+// else if( sum === 21){
+//     console.log("Wohoo! You've got Blackjack! 🥳");
+//     hasBlackJack = false;
+//     console.log(hasBlackJack);
+// }
+// else{
+//     console.log("You're out of the game! 😭");
+// }
